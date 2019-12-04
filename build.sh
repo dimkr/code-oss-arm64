177236d1053ea2627fd4702bc638bcd1c8e882f2
Dima Krasner <dima@dimakrasner.com>
2019-12-04 22:15:35 +0200

    build.sh: reduce RAM usage

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ git clone https://github.com/microsoft/vscode
 cd vscode
 git checkout `git tag | sort -V | grep ^[0-9] | tail -n 1`
 
+sed -e s/4095/1024/g -e s/8192/1024/g -i package.json
+
 yarn
 yarn run gulp vscode-linux-arm64-min
 yarn run gulp vscode-linux-arm64-build-deb
