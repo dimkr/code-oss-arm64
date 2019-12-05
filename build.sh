5cef51fe78616a076406f30436ce2b5ef1b63328
Dima Krasner <dima@dimakrasner.com>
2019-12-05 11:04:18 +0200

    build.sh: minify first

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ git checkout `git tag | sort -V | grep ^[0-9] | tail -n 1`
 sed -e s/4095/1024/g -e s/8192/1024/g -i package.json
 
 yarn
-yarn run gulp vscode-linux-arm64-min
-yarn run gulp vscode-linux-arm64-build-deb
+yarn gulp minify-vscode
+yarn gulp vscode-linux-arm64-min
+yarn gulp vscode-linux-arm64-build-deb
